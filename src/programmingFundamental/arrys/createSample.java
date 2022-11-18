08b0c7d93a68f927e62a4dce5ffb7cdca602b89d
KHSenevirathne <khsenevirathne@gmail.com>
2022-11-19 01:27:00 +0530

    edited

---
+++ src/programmingFundamental/arrys/createSample.java
@@ -0,0 +1,18 @@
+package programmingFundamental.arrys;
+
+import java.util.Arrays;
+
+public class createSample {
+    public static void main(String[] args) {
+
+        int [] num = {11,22,33,44,55,66};
+        int [] num2 = new int[5];
+        int [] num3 = {};
+
+        System.out.println(Arrays.toString(num));
+        System.out.println(Arrays.toString(num2));
+        System.out.println(Arrays.toString(num3));
+
+
+    }
+}
--- src/programmingFundamental/java/PrimaryRange.java
+++ src/programmingFundamental/java/PrimaryRange.java
@@ -4,15 +4,48 @@ import java.util.Scanner;
 
 public class PrimaryRange {
     public static void main(String[] args) {
+        getInput();
+
+    }
+
+    public static void getInput(){
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter Range of Primary Numbers");
-        System.out.print("Enter Start Point :");
+        System.out.println("Get Primary Numbers");
+        System.out.println();
+        System.out.print("Enter the Start Point : ");
         int start = input.nextInt();
-        System.out.print("Enter End Point :");
+        System.out.print("Enter the End Point : ");
         int end = input.nextInt();
 
+        if (start>end){
+            System.out.println("Invalid Input please reEnter..!");
+            getInput();
+        }else{
+            genNum(start,end);
 
+        }
+    }
 
+    public static void genNum(int start, int end){
+
+        for (int i = start; i<=end; i++){
+            if(i>1){
+                chekPrimary(i);
+            }
+        }
+    }
+
+    public static void chekPrimary(int num){
+        int temp = 0;
+        for (int i = num-1; i>1; i--){
+            if (num%i==0){
+                //not primary
+                temp++;
+            }
+        }
+        if (temp == 0 ){
+            System.out.println(num);
+        }
     }
 }
